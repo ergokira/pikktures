04999078e266f401c0346159705a78d05d01fe51
ergokira <josuegb15@gmail.com>
2017-02-06 01:57:19 -0600

    Pikkures v2

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,22 @@
 $(function () {
 
 	'use strict';
-
-	function changeTabs() 
-	{
+	
+	$('.main-nav li a').on('click', function(event) {
+		if (window.matchMedia("(max-width: 760px)").matches) {
+			$(this).closest('.collapse').collapse('toggle');
+		}
 	    $('.main-nav li').removeClass('active-tab');
 	    $('section').children().addClass('hide');
-	    $('.section-' + $(this).attr('id')).removeClass('hide');
-	    $(this).addClass('active-tab');
-	}
+	    $('.section-' + $(this).parent().attr('id')).removeClass('hide');
+	    $(this).parent().addClass('active-tab');
+	});
 
-	$('.main-nav li').on('click', changeTabs);
-
+	//Script to Activate the Carousel
+	$('.carousel').carousel({
+		interval: 3800, // Changes the speed
+		pause:null  // If set to "hover", pauses the cycling of the carousel on mouseenter and resumes the cycling of the carousel on mouseleave. If set to null, hovering over the carousel won't pause it.
+	})
 });
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('serve', ['sass'], function() {
         }
     });
 
-    gulp.watch("app/*.scss", ['sass']);
+    gulp.watch("app/scss/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
     gulp.watch("app/*.js").on('change', browserSync.reload);
 });
